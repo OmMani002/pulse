8c2092aaed011c98ade5c4a349c89efd4a9b02f8
Om Mani Tripathi <maniom002@gmail.com>
2023-12-14 01:19:51 +0530

    MaxWidthWrapper finish

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
 
-export default function MaxWidthWrapper() {
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: ReactNode
+}) => {
   return (
-    <div>MaxWidthWrapper</div>
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+      {children}
+    </div>
   )
 }
+
+export default MaxWidthWrapper
