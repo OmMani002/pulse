3855e4da80dc2c593a7811bd186d6853ae4ab5ff
Om Mani Tripathi <maniom002@gmail.com>
2023-12-22 02:21:29 +0530

    more updates

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -9,11 +9,15 @@ import { Cloud, File } from "lucide-react"
 import { Progress } from "./ui/progress"
 import { resolve } from "path"
 import { useUploadThing } from "@/lib/uploadthing"
+import { useToast } from "./ui/use-toast"
 
 const UploadDropzone = () => {
 
-    const [isUploading, setIsUploading] = useState<boolean>(true)
-    const [uploadProgress, setUploadProgress] = useState<number>(0)
+    const [isUploading, setIsUploading] = 
+        useState<boolean>(true)
+    const [uploadProgress, setUploadProgress] = 
+        useState<number>(0)
+    const { toast } = useToast()
 
     const { startUpload } = useUploadThing("pdfUploader")
 
@@ -49,8 +53,14 @@ const UploadDropzone = () => {
                 const res = await startUpload(acceptedFile)
 
                 if(!res) {
-                    
+                    return toast({
+                        title: 'Something went wrong',
+                        description: 'Please try again later',
+                        variant: 'destructive'
+                    })
                 }
+
+                const [fileResponse] = res
 
                 clearInterval(progressInterval)
                 setUploadProgress(100)
