0ac541aa34eeaaac5cf37b8ed7d65a992503d84d
Om Mani Tripathi <maniom002@gmail.com>
2023-12-24 13:43:31 +0530

    installed react-hook-form package

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,12 +1,18 @@
 "use client"
 
-import { Loader2 } from "lucide-react";
+import { ChevronDown, ChevronUp, Loader2 } from "lucide-react";
 import { Document, Page, pdfjs } from "react-pdf"
-
+{/**installed react-hook-form at 24/12/2023 */}
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 import { useToast } from "./ui/use-toast";
 import {useResizeDetector} from 'react-resize-detector'
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { useState } from "react";
+
+import {useForm} from 'react-hook-form'
+import { z } from "zod";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -20,13 +26,55 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
 
     const {toast} = useToast()
 
+    const [numPages, setNumPages] = useState<number>()
+    const [currPage, setCurrPage] = useState<number>(1)
+
+    const CustomPageValidator = z.object({
+        page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
+    })
+
+    type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+    const {} = useForm({})
+
     const {width, ref} = useResizeDetector()
 
     return (
         <div className="w-full bg-white rounded-md shadow flex flex-col items-center ">
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
                 <div className="flex items-center gap-1.5">
-                    top bar
+                    <Button
+                        disabled= {currPage <= 1}
+                        onClick={() => {
+                            setCurrPage((prev) => 
+                                prev -1 > 1 ? prev - 1 : 1)
+                        }} 
+                        variant= 'ghost' 
+                        aria-label="previous page">
+                        <ChevronDown className="h-4 w-4" />
+                    </Button>
+
+                    <div className="flex items-center gap-1.5">
+                        {/* added a package of shadcn-ui@latest add input here*/}
+                        
+                        <Input className="w-12 h-8"/>
+                        <p className="text-zinc-700 text-sm space-x-1">
+                            <span>/</span>
+                            <span>{numPages ?? 'x'}</span>
+                        </p>
+                    </div>
+                    <Button 
+                        disabled= {
+                            numPages === undefined || 
+                            currPage === numPages
+                        }
+                        onClick={() => {
+                            setCurrPage((prev) => prev + 1 > numPages! ? numPages! : prev + 1)
+                        }}
+                        variant= 'ghost' 
+                        aria-label="next page">
+                        <ChevronUp className="h-4 w-4" />
+                    </Button>
                 </div>
             </div>
             <div className="flex-1 w-full max-h-screen">
@@ -44,8 +92,11 @@ const PdfRenderer = ({url}: PdfRendererProps) => {
                                 variant: 'destructive',
                             })
                         }}
+                        onLoadSuccess={({numPages}) => setNumPages(numPages)}
                         file={url} className='max-h-full'>
-                            <Page width={width ? width: 1} pageNumber={1}/>
+                            <Page 
+                                width={width ? width: 1} 
+                                pageNumber={currPage}/>
                     </Document>
                 </div>
             </div>
